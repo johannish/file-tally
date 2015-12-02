c8e3755128190822cd1424b8fb830d2f94f464ae
LegitMiller <jordan.kay@gmail.com>
2015-12-01 21:30:33 -0700

    created a dummy template in render using mustache

--- service/render.tcl
+++ service/render.tcl
@@ -1,3 +1,5 @@
+package require mustache
+
 namespace eval ::render {
 	set fp [open "template/file-details.html" r]
 	variable fileDetailsTmpl [read $fp]
@@ -17,7 +19,20 @@ proc ::render::index {} {
 }
 
 proc ::render::fileDetails {fileId} {
-	variable fileDetailsTmpl
+	variable fileDetailsTmpl ;# ?
 	set fileMetadata [::repo::getobject uploads $fileId]
-	return [::simp-templer::render $fileMetadata $fileDetailsTmpl]
+	set template {
+		<div>id: {{id}}</div>
+		<div>name: {{name}}</div>
+		<div>votes: {{votes}}</div>
+		<div>type: {{type}}</div>
+		<div>description: {{description}}</div>
+		<div>tags: {{tags}}</div>
+		<div>blob: {{blob}}</div>
+		<div>uploader: {{uploader}}</div>
+		<div>programs_id: {{programs_id}}</div>
+		<div>created_at: {{created_at}}</div>
+		<div>modified_at: {{modified_at}}</div>
+	}
+	return [::mustache::mustache $template $fileMetadata]
 }
